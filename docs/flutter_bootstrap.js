a50b8d39c7977863728d1a7513b08524cbdf6f3b
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-14 15:15:38 +0300

    fix: Final build to /docs folder without web-renderer flag

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3298001546"
+    serviceWorkerVersion: "30520695"
   }
 });
