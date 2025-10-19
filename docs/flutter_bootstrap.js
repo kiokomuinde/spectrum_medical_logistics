c0b29fb6d257f93c514bdf933d78dfb51d8c51bf
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-19 14:29:01 +0300

    Initialized navbar related screens.

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "30520695"
+    serviceWorkerVersion: "2871652591"
   }
 });
